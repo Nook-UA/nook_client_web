507f1dc5aefce98ee619abc82914af3fb7aa0c08
fungame2270 <tomasvictal@gmail.com>
2024-10-14 01:19:52 +0100

    trying to fix auth
    Co-authored-by: Daniel Madureira <daniel.madureira@ua.pt>

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -3,14 +3,12 @@ import CredentialsProvider from "next-auth/providers/credentials";
 const options = {
     providers: [
         CredentialsProvider({
-            name:"Credentials",
-            credentials:{
-                username:{label: "Username", type: "text"},
-                password:{label: "Password", type: "password"}
-            },
+            credentials: {
+                username: { label: "Username", type: "text"},
+                password: { label: "Password", type: "password"},
+              },
             async authorize(credentials,req){
-
-                const dummyUser = {id:1, name: "admin", password: "admin2024"}
+                console.log("chamei")
 
                 const response = await fetch(
                     `http://localhost:8000/api/auth/login?username=${credentials.username}&password=${credentials.password}`,
@@ -23,21 +21,21 @@ const options = {
                 }
 
                 const body = await response.json()
-                console.log(body)
                 return {
                     name: body.username
                 };
                 
             },
-            pages: {
-                signIn: '/signin',
-                // signOut: '/auth/signout',
-                // error: '/auth/error', // Error code passed in query string as ?error=
-                // verifyRequest: '/auth/verify-request', // (used for check email message)
-                // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-              }
         })
     ],
+    pages: {
+        signIn: '/auth/signin',
+        // signOut: '/auth/signout',
+        // error: '/auth/error', // Error code passed in query string as ?error=
+        // verifyRequest: '/auth/verify-request', // (used for check email message)
+        // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+    },
+    debug:true,
 
 }
 
--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -1,13 +1,14 @@
-import { signIn } from "next-auth/react";
 import { FaLock,FaUserLarge } from "react-icons/fa6";
 import { getCsrfToken } from "next-auth/react";
 
-export default async function SignIn() {
-    const csrfToken = await getCsrfToken();
+export default function SignIn() {
+    const csrfToken = getCsrfToken();
+
     return(
         <div className="flex flex-col font-bold items-center h-full p-20 text-4xl gap-5">
             <p>Login to Your Account</p>
             <form method="post" action="/api/auth/callback/credentials" className="flex flex-col w-[90%] gap-5 text-3xl">
+                <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
                 <div>
                     <p>Username</p>
                     <label className="bg-slate-200 rounded-[20px] text-lg p-1 px-3 flex flex-row items-center mt-1">
