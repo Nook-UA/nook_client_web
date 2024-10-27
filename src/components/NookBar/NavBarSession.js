b69d7dfdd9e81c7e4dac9d8219c32e3c333a31d7
fungame2270 <tomasvictal@gmail.com>
2024-10-27 11:35:52 +0000

    added url to env

--- src/components/NookBar/NavBarSession.js
+++ src/components/NookBar/NavBarSession.js
@@ -10,7 +10,7 @@ import { signOut, useSession } from "next-auth/react";
         signOut({
             redirect: false,
         }).then(() => {
-            window.location.href = "https://pao.auth.us-east-1.amazoncognito.com/logout?client_id=4foftjuvb0oto1qbs0db3jlbha&logout_uri=http%3A%2F%2Flocalhost%3A3000%2F"
+            window.location.href = process.env.LOGOUT_URL
         });
     }
     return(
