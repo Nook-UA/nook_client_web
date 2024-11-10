bbab5b9500d498c4c3caa5f0ecb2f2d59a8d4a5d
João Andrade <31666693+WildBunnie@users.noreply.github.com>
2024-11-10 17:52:56 +0000

    [Esnook 60] Parking Lot info page (#3)
    * added login function with credential dummy user
    
    * added nextUi and navbar
    
    * update login to work with navbar
    
    * update login to access api
    
    * custom login page layout done
    
    * trying to fix auth
    
    Co-authored-by: Daniel Madureira <daniel.madureira@ua.pt>
    
    * ESNOOK-35 created park page with api data
    
    * very barebones version of the park info page
    
    * changed layout for the data to better fit the page and made it load on the server side instead of client side
    
    ---------
    
    Co-authored-by: fungame2270 <tomasvictal@gmail.com>
    Co-authored-by: Daniel Madureira <daniel.madureira@ua.pt>

---
+++ src/app/parks/[park_id]/page.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Image } from "@nextui-org/react";
+
+export default async function Page({params}) {
+  const {park_id} = params;
+  const parkData = await fetch(`http://localhost:5000/parking_lot/${park_id}`, {
+    method: "GET",
+    cache:"no-store"
+  }).then((res) => res.json());
+
+  console.log("========================================================");
+  console.log(park_id);
+  console.log(parkData);
+  console.log("========================================================");
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-gray-900 w-full">
+      <h1 className="text-5xl font-bold text-[#FFAC75] mb-6">Parking Lot Details</h1>
+      
+      <div className="w-full flex justify-center">
+        <Image
+          alt={`${park_id} image`}
+          className="w-full h-[500px] object-cover"
+          src={`http://localhost:5000${parkData.image_url}`}
+        />
+      </div>
+      
+      <div className="w-full flex flex-col items-center py-6 bg-white">
+        <h2 className="text-3xl font-semibold mb-2">Parking Lot ID: {park_id}</h2>
+        <div className="text-lg font-light text-gray-700 text-center w-full max-w-3xl">
+          <p className="mb-1">Total Spaces: {parkData.occupancy.total}</p>
+          <p className="mb-1">Occupied: {parkData.occupancy.occupied}</p>
+          <p>Free: {parkData.occupancy.freed}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/parks/layout.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import NookLogo from "@/assets/logo_sm.svg";
+
+export default function layout({ children }) {
+  return (
+    <div className="flex flex-row justify-start">
+      <div className="max-m-[5%] bg-[#FFAC75] min-h-screen flex flex-col items-center">
+        <Image
+          priority
+          src={NookLogo}
+          alt="Nook Logo"
+          className="w-[7rem] aspect-square p-4"
+        />
+      </div>
+      <div className="grow">{children}</div>
+    </div>
+  );
+}
