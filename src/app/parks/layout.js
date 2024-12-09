6aa6b1e1cb33842db80959cc81aa63cd7e0fc34a
fungame2270 <tomasvictal@gmail.com>
2024-12-09 16:33:46 +0000

    added nookbar to layout

--- src/app/parks/layout.js
+++ src/app/parks/layout.js
@@ -1,10 +1,10 @@
-import NookNavbar from "@/components/NookBar/NookNavbar";
+import NookNavbar from "@/components/NookBar";
 
 
 export default function layout({ children }) {
   return (
     <div className="flex flex-row justify-start">
-      
+      <NookNavbar />
       <div className="grow w-[70%]">{children}</div>
     </div>
   );
