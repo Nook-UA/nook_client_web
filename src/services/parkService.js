26c740a8f895d6ea7e8a6b28122a35dbbce22d67
fungame2270 <tomasvictal@gmail.com>
2024-12-09 16:35:11 +0000

    added park service

---
+++ src/services/parkService.js
@@ -0,0 +1,21 @@
+import { getServerSession } from "next-auth/next";
+import options from "@/app/api/auth/[...nextauth]/options";
+
+const token = await getServerSession(options).then((res) => res?.user?.idToken);
+
+const headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + token,
+}
+
+const ParkService = {
+    getParks: async () => {
+        const parks = await fetch(`${process.env.BACKEND_URL}/park`, {
+            method: "GET",
+            headers: headers
+        })
+        return parks;
+    }
+}
+
+export default ParkService;
