f80aa378215c6c88d59820e18a2e0675b5e9e3fb
fungame2270 <tomasvictal@gmail.com>
2024-12-09 14:59:42 +0000

    remove old navbar from the layout of parks

--- src/app/parks/layout.js
+++ src/app/parks/layout.js
@@ -1,17 +1,10 @@
-import Image from "next/image";
-import NookLogo from "@/assets/logo_sm.svg";
+import NookNavbar from "@/components/NookBar/NookNavbar";
+
 
 export default function layout({ children }) {
   return (
     <div className="flex flex-row justify-start">
-      <div className="max-m-[5%] bg-[#FFAC75] min-h-screen flex flex-col items-center">
-        <Image
-          priority
-          src={NookLogo}
-          alt="Nook Logo"
-          className="w-[7rem] aspect-square p-4"
-        />
-      </div>
+      
       <div className="grow w-[70%]">{children}</div>
     </div>
   );
