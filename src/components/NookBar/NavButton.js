848f99bb4ab6de68aa4ac440645d72d44a71f553
fungame2270 <tomasvictal@gmail.com>
2024-12-09 16:35:00 +0000

    change from div to button to have interation

--- src/components/NookBar/NavButton.js
+++ src/components/NookBar/NavButton.js
@@ -6,12 +6,12 @@ import { motion } from "framer-motion"
 function NavButton({children, props} ) {
     return (
         <Link href={props.href} className="flex items-center gap-2">
-        <motion.div className="flex items-center gap-1"
+        <motion.button className="flex items-center gap-1"
             whileHover={{ scale: 1.1,x: 10 }}
             whileTap={{ scale: 0.9 }}
           >
             {children}
-          </motion.div>
+          </motion.button>
         </Link>
     )
 }
