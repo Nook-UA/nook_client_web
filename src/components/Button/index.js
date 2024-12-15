6443155370502efe4c426a0bada76a267a682fe1
fungame2270 <tomasvictal@gmail.com>
2024-12-15 22:57:40 +0000

    added justify center to button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,9 @@
 "use client";
 import { motion } from "framer-motion";
 
-function Button({children, className}) {
+function Button({children, className,onClick}) {
     return (
-        <motion.button className={`${className} flex items-center gap-2`}
+        <motion.button className={`${className} flex justify-center items-center gap-2`} onClick={onClick}
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.9 }}
         >
