f4d91804db012f2ba012f37a9bd49a7405682e7a
Tomas Victal <96839619+fungame2270@users.noreply.github.com>
2025-01-17 12:53:05 +0000

    added deploy fixes to dev (#8)
    * fixed hardcoded urls and runtime env variables
    
    * added NEXT_PUBLIC
    
    * added url disable dashboard
    
    ---------
    
    Co-authored-by: WildBunnie <wildbunnie2003@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import NookNavbar from "@/components/NookBar";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-row">
       <NookNavbar/>
       <div className="">
-        hola
       </div>
     </div>
   );
--- src/app/parks/[park_id]/spots/page.js
+++ src/app/parks/[park_id]/spots/page.js
@@ -11,7 +11,7 @@ export default async function Page({params}) {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen text-gray-900 w-full">
             <h1 className="text-5xl font-bold text-[#FFAC75] mb-6">Spots for park {park_id}</h1>
-            <InteractiveSpotPicker imageUrl={`${process.env.NEXT_PUBLIC_PARKING_DETECTION_URL}`+parkInfo.image_url} park_id={park_id} />
+            <InteractiveSpotPicker imageUrl={`${process.env.NEXT_PUBLIC_PARKING_DETECTION_URL}`+parkInfo.image_url} park_id={park_id} url={process.env.NEXT_PUBLIC_PARKING_DETECTION_URL} />
         </div>
     )
 }
